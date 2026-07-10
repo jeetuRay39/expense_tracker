3e79f6a46703ef82786289e2457a4dd43e6f7c2c
jeetury39 <jeetu.ray39@gmail.com>
2026-07-10 09:18:31 +0545

    Initial Django setup with custom gitignore

---
+++ config/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+
+   path(
+       'admin/',
+       admin.site.urls
+   ),
+
+   path(
+       'api/auth/',
+       include('users.api.urls')
+   ),
+
+   path(
+       'api/',
+       include('expenses.api.urls')
+   ),
+]
